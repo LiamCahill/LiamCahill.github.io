c4ae65653823177f2abfc6377d9f67e1d9764e26
Liam Cahill <cahill.liam220@gmail.com>
2023-02-02 10:27:51 -0500

    Added template

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "Python PDF Converter",
+    subtitle: "Automating JSON form creation",
+    description:
+      "Coded a solution to automate the creation of JSON forms from Excel/PDF files. Increased productivity by removing the need for forms to be hard-coded.",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Next Book",
+    subtitle: "Python",
+    description:
+      "Coded a simple project to find and buy my next book. The program takes a book from my current list at random, launches Google Chrome, and searches for it on Amazon.",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = ["Java", "Python", "SQL", "React"];
